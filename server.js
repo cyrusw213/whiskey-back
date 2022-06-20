1c0588219a61cdfd670788d0d7b71bc512dc5f47
hudnellmarcus <hudnell.marcus@gmail.com>
2022-06-20 04:23:03 -0700

    added user token code. no functional changes it is commented out.

--- server.js
+++ server.js
@@ -2,8 +2,17 @@
 ////////////////////////////////////////////////////////
 const express = require('express');
 const app = express(); 
-require('dotenv').config();
 const whiskeyController = require('./controllers/whiskeys')
+const admin = require('firebase-admin'); 
+
+// CONFIGURE APP SETTINGS ////////////////////
+require('dotenv').config();
+
+const serviceAccount = require('./firebase-service-key.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+}); 
 
 const { PORT, MONGODB_URI } = process.env; 
 
@@ -37,6 +46,28 @@ app.use(cors()); // Access-Control-Allow
 app.use(morgan('dev'));
 app.use(express.json());//this creates req.body
 app.use(whiskeyController)
+
+// AUTHORIZATION MIDDLEWARE ////////////////
+// creates user and authenticates user token
+// app.use(async (req, res, next) => {
+//     const token = req.get('Authorization');
+//     if (token) {
+//         console.log(token); 
+//         const user = await admin.auth().verifyIdToken(token.replace('Bearer ', ""));
+//         req.user = user; 
+//     } else {
+//         req.user = null;
+//     }
+//     next();
+// });
+// // check if req.user exists (is user authenticated?)
+// function isAuthenticated(req, res, next) {
+//     if (!req.user) {
+//         return res.status(401).json({ message: 'you must be logged in to save favorite'}); 
+//     } else {
+//         return next();
+//     };
+// }; 
 
 
 app.listen(PORT, () => console.log(`I love you ${PORT}`)); 
